5c2db77ae3ea9b0e3dc676ee183b48f933bef43a
derishev <102142040+derishev@users.noreply.github.com>
2022-10-14 10:48:57 +0300

    Домашнее задание к 4 уроку
    Деришев Алексей
    Пайтон разработка
    27 сентября старт курса
    Домашнее задание к 4 уроку

---
+++ task3.py
@@ -0,0 +1,49 @@
+"""
+Задание 3.
+Приведен код, формирующий из введенного числа
+обратное по порядку входящих в него
+цифр и вывести на экран.
+Сделайте профилировку каждого алгоритма через timeit
+Обязательно предложите еще свой вариант решения!
+Сделайте вывод, какая из четырех реализаций эффективнее и почему!
+"""
+from timeit import timeit
+
+def revers(enter_num, revers_num=0):
+    if enter_num == 0:
+        return
+    else:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+        revers(enter_num, revers_num)
+
+
+def revers_2(enter_num, revers_num=0):
+    while enter_num != 0:
+        num = enter_num % 10
+        revers_num = (revers_num + num / 10) * 10
+        enter_num //= 10
+    return revers_num
+
+
+def revers_3(enter_num):
+    enter_num = str(enter_num)
+    revers_num = enter_num[::-1]
+    return revers_num
+
+def revers_4(enter_num, revers_num = ''):
+    for i in str(enter_num):
+        revers_num = i + revers_num
+    return revers_num
+
+enter_num = 123456789
+
+print(timeit(stmt="revers(enter_num)", setup="from __main__ import revers, enter_num", number=1000))
+print(timeit(stmt="revers_2(enter_num)", setup="from __main__ import revers_2, enter_num", number=1000))
+print(timeit(stmt="revers_3(enter_num)", setup="from __main__ import revers_3, enter_num", number=1000))
+print(timeit(stmt="revers_4(enter_num)", setup="from __main__ import revers_4, enter_num", number=1000))
+
+"""
+Самая эффективная 3 реализация, потому как данный алгоритм затращивает минимальное время на исполнение
+"""
